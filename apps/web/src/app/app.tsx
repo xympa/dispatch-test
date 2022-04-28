1a4c0260a7caa4bb4d6a9b4819d3fe9808f28032
Pedro Nascimento <pedronascimento@enso-origins.com>
2022-04-28 21:49:26 +0100

    has extra message

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,7 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 export function App() {
-  return <div>This is a welcome to {process.env['NX_NAME']}</div>;
+  return (
+    <div>
+      This is a welcome to {process.env['NX_NAME']},{' '}
+      {process.env['NX_EXTRA_MESSAGE']}
+    </div>
+  );
 }
 
 export default App;
